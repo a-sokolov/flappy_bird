9a191ccd5dd5089d83ff61e848b8f9ea185d5aad
Anatoly Sokolov <AViSokolov@sberbank.ru>
2021-01-12 20:12:09 +0300

    fix

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -3,11 +3,13 @@ import { Scene } from '../scene'
 import { DrawFloor } from './parts/draw-floor'
 import { DrawScore } from './parts/draw-score'
 
-import { ImageType } from '../interfaces'
+import { HotKeys, ImageType } from '../interfaces'
 
 export class Menu extends Scene {
   constructor(game) {
     super(game)
+
+    this.startGame = this.startGame.bind(this)
   }
 
   init() {
@@ -15,16 +17,24 @@ export class Menu extends Scene {
 
     this.addScenePart(new DrawFloor(this))
     this.addScenePart(new DrawScore(this))
+
+    this.game.control.addListener(HotKeys.JUMP, this.startGame)
+  }
+
+  destroy() {
+    super.destroy()
+
+    this.game.control.removeListener(HotKeys.JUMP, this.startGame)
+  }
+
+  startGame() {
+    this.finish(Scene.START_GAME)
   }
 
   render(time) {
     this.game.screen.drawImage(ImageType.backgroundDay)
     this.game.screen.drawImageByCenter(ImageType.menu)
     this.game.screen.printByXCenter(`high score: ${this.game.highScore}`, this.game.screen.height - 80)
-
-    if (this.game.control.jump) {
-      this.finish(Scene.START_GAME)
-    }
 
     super.render(time)
   }
